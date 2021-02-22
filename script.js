1759e7e27be5b15d4f45dfaf58959f2be0fb7cad
lai2 <informatica@lai2>
2021-02-22 11:45:19 +0100

    jquery done

--- script.js
+++ script.js
@@ -10,14 +10,11 @@ function loadJavaDoc() {
   }
 
   function loadJqueryDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("jquerydemo").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "http://api.mathjs.org/v4/?expr=2*(7-3)", true);
-    xhttp.send();
+    $(document).ready(function(){
+          $.ajax({url: "http://api.mathjs.org/v4/?expr=2*(7-3)", success: function(result){
+            $("#jquerydemo").html(result);
+          }});
+      });
   }
 
 
