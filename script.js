16cf7509b9fa70e9a8e41f24f49b07cf921efa2b
lai2 <informatica@lai2>
2021-02-22 12:28:46 +0100

    POST attempt

--- script.js
+++ script.js
@@ -25,5 +25,21 @@ function loadJavaDoc() {
       });
   }
 
+  function POSTloadJavaDoc() {
+    var value = document.getElementById("POSTjsinput").value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("POSTjavascriptdemo").innerHTML = value + "=" +this.responseText;
+      }
+      else document.getElementById("POSTjavascriptdemo").innerHTML = "Errore durante il calcolo (controllare espressione)";
+    };
+    xhttp.open("POST", "http://api.mathjs.org/v4/", false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("Access-Control-Allow-Headers", "*");
+    console.log('{"expr": ['+ value +']}');
+    xhttp.send('{"expr": ['+ value +']}');
+  }
+
 
   
