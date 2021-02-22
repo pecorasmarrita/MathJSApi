e8788f4e3182dd8ec309de11b517e4123fafffb3
lai2 <informatica@lai2>
2021-02-22 11:33:21 +0100

    javascript done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function loadDoc() {
+function loadJavaDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -8,4 +8,6 @@ function loadDoc() {
     xhttp.open("GET", "http://api.mathjs.org/v4/?expr=2*(7-3)", true);
     xhttp.send();
   }
+
+
   
