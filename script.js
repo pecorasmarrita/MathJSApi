7ffd762fe2bd5fe88bccbd696eee7caa9e3eaaec
lai2 <informatica@lai2>
2021-02-22 11:35:55 +0100

    javascript done

--- script.js
+++ script.js
@@ -9,5 +9,16 @@ function loadJavaDoc() {
     xhttp.send();
   }
 
+  function loadJqueryDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("jquerydemo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "http://api.mathjs.org/v4/?expr=2*(7-3)", true);
+    xhttp.send();
+  }
+
 
   
